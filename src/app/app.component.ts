941185ba90011254619b651bad7cb3a5c505f799
Roberbot <marcianorober@gmail.com>
2022-10-28 08:44:13 +0200

    Subiendo reloj digital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  hora:any;
+  minuto:any;
+
   title = 'reloj_proyect';
+
+  ngOnInit(){
+    
+    setInterval(()=>{
+      this.relojDigital();
+    },1000);
+ 
+  }
+
+  relojDigital(){
+    this.hora = new Date();
+    this.minuto = new Date();
+
+  }
+
+ 
+
 }
